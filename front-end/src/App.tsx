41256fd59e53d8fe5b4adc618130e0f3d9f1b772
Eirene Fireman <eof@ic.ufal.br>
2021-09-10 19:50:06 -0300

    add padrão white/orange-box em DesmarcarFinal

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import Login from './pages/Login/Login';
 import Home from './pages/Home/Home';
+import DesmarcarFinal from './pages/DesmarcarFinal/DesmarcarFinal';
 
 function App() {
   return (
     <>
-      <Login/>
+      <DesmarcarFinal/>
     </>
   );
 }
