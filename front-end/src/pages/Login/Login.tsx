00a42b7cbae5e5d04394ca96a15512e2818a787e
Eirene Fireman <eof@ic.ufal.br>
2021-09-10 14:44:53 -0300

    iniciando estilização da tela de login

---
+++ front-end/src/pages/Login/Login.tsx
@@ -0,0 +1,52 @@
+import React from "react";
+import { useForm } from "react-hook-form";
+
+import './style.css' 
+import { Button } from "../../components/Button";
+import logo from '../../assets/images/logo-fcamara.png'
+import ilustracaoLogin from '../../assets/images/ilustracao-login.png'
+
+type FormData = {
+    email: string;
+    password: string;
+  }
+
+const Login = () => {
+    const { register, handleSubmit } = useForm<FormData>();
+
+    const onSubmit = (formData: FormData) => {
+      console.log(formData)
+    }
+      
+    return (
+        <>
+            <div className="white-box">
+                <h1><img className="logo" src={logo}></img></h1>
+                <form className="login-form" onSubmit={handleSubmit(onSubmit)}>
+                    <p>Login</p>
+                    <input 
+                        {...register("email")}
+                        name="email" 
+                        type="email" 
+                        placeholder="E-mail"/>
+                    <p>Senha</p>
+                    <input 
+                        {...register("password")}
+                        name="password" 
+                        type="password" 
+                        placeholder="Senha"/>
+                    <Button type='submit'>Entrar</Button>
+                    {/* <button>Entrar</button> */}
+                </form>
+            </div>
+            <div className="orange-box">
+                <div className="orange-box-content">
+                    <p className="orange-box-text">Estamos aqui para facilitar o agendamento da sua ida ao escritório, fazendo com que ele se torne rápido e seguro para todos!</p>
+                    <img className="ilustracao-login" src={ilustracaoLogin}></img>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default Login;
