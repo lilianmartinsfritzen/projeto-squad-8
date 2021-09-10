d9a3a6df0029ed461af4af746e7493d3c34f826f
Eirene Fireman <eof@ic.ufal.br>
2021-09-10 17:56:18 -0300

    colocando componente de home apenas p visualização

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import Login from './pages/Login/Login';
+import Home from './pages/Home/Home';
 
 function App() {
   return (
     <>
-      <Login/>
+      <Home/>
     </>
   );
 }
