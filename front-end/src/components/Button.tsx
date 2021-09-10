503814c24cbadd2d04fb5a978fd96eee75ec46ec
Eirene Fireman <eof@ic.ufal.br>
2021-09-10 14:44:21 -0300

    add componente de botão principal

---
+++ front-end/src/components/Button.tsx
@@ -0,0 +1,15 @@
+import { ButtonHTMLAttributes } from "react";
+import "./styles.css";
+
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
+  isOutlined?: boolean
+}
+
+export function Button({ isOutlined = false, ...props }: ButtonProps) {
+  return (
+    <button 
+      className={`button ${isOutlined ? 'outlined' : ''}`}
+      {...props} 
+    />
+  )
+}
