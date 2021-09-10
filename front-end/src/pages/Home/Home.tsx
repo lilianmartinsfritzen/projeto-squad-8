e78ac387505ff2f38b8b74aec7b6ae5a9337059e
Eirene Fireman <eof@ic.ufal.br>
2021-09-10 18:04:04 -0300

    estliziando Home com padrão white-box|orange-box

--- front-end/src/pages/Home/Home.tsx
+++ front-end/src/pages/Home/Home.tsx
@@ -1,11 +1,26 @@
 import React from 'react';
 
+import './style.css' 
 import { Button } from "../../components/Button";
+import logo from '../../assets/images/logo-fcamara.png'
+import ilustracaoLogin from '../../assets/images/ilustracao-home.png'
 
 
 const Home = () => {
     return(
-        <h1>Home</h1>
+        <>
+            <aside className="white-box">
+                <h1><img className="logo" src={logo}></img></h1>
+                <div>
+                {/* <Button className="button" type='submit'>Entrar</Button> */}
+                </div>
+            </aside>
+            <main className="orange-box">
+                <div className="orange-box-content">
+                    <img className="ilustracao-home" src={ilustracaoLogin}></img>
+                </div>
+            </main>
+        </>
     );
 }
 
