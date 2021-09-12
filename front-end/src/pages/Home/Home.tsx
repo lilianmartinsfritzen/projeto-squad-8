f8fb067776f9618f2c7c08e398042ae37c3a1bfa
Eirene Fireman <eof@ic.ufal.br>
2021-09-12 00:23:01 -0300

    ajustando semântica html da página de home

--- front-end/src/pages/Home/Home.tsx
+++ front-end/src/pages/Home/Home.tsx
@@ -8,21 +8,21 @@ import ilustracaoHome from '../../assets/images/ilustracao-home.svg'
 
 const Home = () => {
     return(
-        <>
-            <aside className="white-box">
+        <main className="main">
+            <section className="white-box">
                 <h1><img className="logo" src={logo}></img></h1>
                 <div className="white-box-content">
                 <Button className="button" type='submit'>Agendamento</Button>
                 <Button className="button" type='submit'>Desmarcar agendamento</Button>
                 <Button className="button" type='submit'>Relatórios (em breve)</Button>
                 </div>
-            </aside>
-            <main className="orange-box">
+            </section>
+            <section className="orange-box">
                 <div className="orange-box-content">
                     <img className="ilustracao-home" src={ilustracaoHome}></img>
                 </div>
-            </main>
-        </>
+            </section>
+        </main>
     );
 }
 
